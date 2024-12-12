6868f035636d98166ae995cc57ffc5065c7d71a7
wiqin21 <waqasfrom115666@gmail.com>
2024-12-12 10:52:11 +0500

    Create area.js

---
+++ area.js
@@ -0,0 +1,9 @@
+function display(){
+    event.preventDefault();
+    let I1 = document.getElementById("length").value;
+    let I2 = document.getElementById("width").value;
+
+    let I3=I1*I2;
+    // alert(I3)
+     document.getElementById("area").value=I3;
+}
